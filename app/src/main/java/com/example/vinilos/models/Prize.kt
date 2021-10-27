a1fc71efd804b584fa99a0c9948eaa8913c88923
johanCarvajalAndes <j.carvajalm@uniandes.edu.co>
2021-10-27 18:30:55 -0500

    repositorioInicial

---
+++ app/src/main/java/com/example/vinilos/models/Prize.kt
@@ -0,0 +1,8 @@
+package com.example.vinilos.models
+
+data class Premio (
+    val organitation:String,
+    val name:String,
+    val description:String
+
+)
---
+++ app/src/main/java/com/example/vinilos/viewmodels/PrizeViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.vinilos.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class PremioViewModel (application: Application) :  AndroidViewModel(application) {
+}

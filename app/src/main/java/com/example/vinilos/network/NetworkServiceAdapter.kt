fb9ac73e37e354f675c2cad7955fd2e782b9642e
johanCarvajalAndes <j.carvajalm@uniandes.edu.co>
2021-11-05 13:53:48 -0500

    mejor a la arquitectura del proyecto

---
+++ app/src/main/java/com/example/vinilos/network/NetworkServiceAdapter.kt
@@ -0,0 +1,66 @@
+package com.example.getonetutorial.network
+
+import android.content.Context
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.getonetutorial.models.Album
+import com.example.getonetutorial.models.Comments
+import com.example.getonetutorial.models.Performers
+import com.example.getonetutorial.models.Tracks
+import org.json.JSONArray
+import org.json.JSONObject
+
+class NetworkServiceAdapter constructor(context: Context) {
+    companion object{
+        const val BASE_URL= "https://vinyl-miso.herokuapp.com/"
+        var instance: NetworkServiceAdapter? = null
+        fun getInstance(context: Context) =
+            instance ?: synchronized(this) {
+                instance ?: NetworkServiceAdapter(context).also {
+                    instance = it
+                }
+            }
+    }
+    private val requestQueue: RequestQueue by lazy {
+        // applicationContext keeps you from leaking the Activity or BroadcastReceiver if someone passes one in.
+        Volley.newRequestQueue(context.applicationContext)
+    }
+
+
+    fun getOneAlbum(albumId:Int, onComplete:(resp: Album)->Unit, onError: (error:VolleyError)->Unit) {
+        requestQueue.add(getRequest("albums/$albumId",
+            Response.Listener<String> { response ->
+                val resp = JSONObject(response)
+                var item:JSONObject? = null
+
+                item = resp
+                Log.d("Response", item.toString())
+                onComplete(Album(id= item.getInt("id"),
+                    name = item.getString("name"),
+                    cover = item.getString("cover"),
+                    releaseDate = item.getString("releaseDate"),
+                    description = item.getString("description"),
+                    genre = item.getString("genre"),
+                    recordLabel = item.getString("recordLabel")))
+            },
+            Response.ErrorListener {
+                onError(it)
+            }))
+    }
+
+    private fun getRequest(path:String, responseListener: Response.Listener<String>, errorListener: Response.ErrorListener): StringRequest {
+        return StringRequest(Request.Method.GET, BASE_URL+path, responseListener,errorListener)
+    }
+    private fun postRequest(path: String, body: JSONObject,  responseListener: Response.Listener<JSONObject>, errorListener: Response.ErrorListener ):JsonObjectRequest{
+        return  JsonObjectRequest(Request.Method.POST, BASE_URL+path, body, responseListener, errorListener)
+    }
+    private fun putRequest(path: String, body: JSONObject,  responseListener: Response.Listener<JSONObject>, errorListener: Response.ErrorListener ):JsonObjectRequest{
+        return  JsonObjectRequest(Request.Method.PUT, BASE_URL+path, body, responseListener, errorListener)
+    }
+}

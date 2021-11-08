5cdc839da2e81ce6595388f471e4f35eea6702c6
Elkin Rativa <elkinming@gmail.com>
2021-11-07 22:35:06 -0500

    Pruebas con espresso 2

---
+++ app/src/androidTest/java/com/example/vinilos/ui/Test_Crear_Album_2.kt
@@ -0,0 +1,220 @@
+package com.example.vinilos.ui
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onData
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import com.example.vinilos.R
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.*
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class Test_Crear_Album_2 {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun test_Crear_Album_2() {
+        val materialButton = onView(
+            allOf(
+                withId(R.id.button_collector_rol), withText("COLECCIONISTA"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("android.widget.RelativeLayout")),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton.perform(click())
+
+        val materialButton2 = onView(
+            allOf(
+                withId(R.id.button_albums_menu), withText("ALBUMES"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("android.widget.RelativeLayout")),
+                        0
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton2.perform(click())
+
+        val floatingActionButton = onView(
+            allOf(
+                withId(R.id.fab_prizes),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        floatingActionButton.perform(click())
+
+        val appCompatEditText = onView(
+            allOf(
+                withId(R.id.editTextName),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                        0
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText.perform(replaceText("test"), closeSoftKeyboard())
+
+        val appCompatEditText2 = onView(
+            allOf(
+                withId(R.id.editTextCover),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                        0
+                    ),
+                    4
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText2.perform(replaceText("test"), closeSoftKeyboard())
+
+        val appCompatSpinner = onView(
+            allOf(
+                withId(R.id.spinner_genre),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                        0
+                    ),
+                    8
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatSpinner.perform(click())
+
+//        val appCompatCheckedTextView = onData(anything())
+//            .inAdapterView(
+//                childAtPosition(
+//                    withClassName(`is`("android.widget.PopupWindow$PopupBackgroundView")),
+//                    0
+//                )
+//            )
+//            .atPosition(3)
+//        appCompatCheckedTextView.perform(click())
+
+        val appCompatSpinner2 = onView(
+            allOf(
+                withId(R.id.spinner_record_label),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                        0
+                    ),
+                    10
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatSpinner2.perform(click())
+
+//        val appCompatCheckedTextView2 = onData(anything())
+//            .inAdapterView(
+//                childAtPosition(
+//                    withClassName(`is`("android.widget.PopupWindow$PopupBackgroundView")),
+//                    0
+//                )
+//            )
+//            .atPosition(3)
+//        appCompatCheckedTextView2.perform(click())
+
+        val appCompatEditText3 = onView(
+            allOf(
+                withId(R.id.editTextTextDescription),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                        0
+                    ),
+                    12
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText3.perform(replaceText("test"), closeSoftKeyboard())
+
+        val materialButton3 = onView(
+            allOf(
+                withId(R.id.buttonCreateAlbum), withText("CREAR ALBUM"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                        0
+                    ),
+                    13
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton3.perform(click())
+
+        val materialButton4 = onView(
+            allOf(
+                withId(android.R.id.button1), withText("OK"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.buttonPanel),
+                        0
+                    ),
+                    3
+                )
+            )
+        )
+        materialButton4.perform(scrollTo(), click())
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}

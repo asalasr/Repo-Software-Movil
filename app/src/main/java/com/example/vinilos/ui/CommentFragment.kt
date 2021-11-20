96f71a0e476120dd31cb3eb5aea8b2755a7e4899
johanCarvajalAndes <j.carvajalm@uniandes.edu.co>
2021-11-20 10:39:36 -0500

    comentarios creados

--- app/src/main/java/com/example/vinilos/ui/CommentFragment.kt
+++ app/src/main/java/com/example/vinilos/ui/CommentFragment.kt
@@ -1,60 +1,83 @@
 package com.example.vinilos.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.vinilos.R
+import com.example.vinilos.databinding.FragmentCommentBinding
+import com.example.vinilos.databinding.FragmentPrizeBinding
+import com.example.vinilos.models.Comment
+import com.example.vinilos.models.Prize
+import com.example.vinilos.ui.adapters.CommentAdapter
+import com.example.vinilos.ui.adapters.PrizeAdapter
+import com.example.vinilos.viewmodels.CommentViewModel
+import com.example.vinilos.viewmodels.PrizeViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CommentFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CommentFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentCommentBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewModel: CommentViewModel
+    private var viewModelAdapter: CommentAdapter? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_comment, container, false)
+        _binding = FragmentCommentBinding.inflate(inflater, container, false)
+        val view = binding.root
+        viewModelAdapter = CommentAdapter()
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CommentFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CommentFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerView = binding.fragmentsRvcomments
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = viewModelAdapter
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val activity = requireNotNull(this.activity) {
+            "You can only access the viewModel after onActivityCreated()"
+        }
+        Log.i("titulo",""+activity.actionBar?.title)
+        activity.actionBar?.title = getString(R.string.prizes)
+        //val args: CommentFragmentArgs by navArgs()
+        //Log.d("ArgsCommebt", args.albumId.toString())
+        viewModel = ViewModelProvider(this, CommentViewModel.Factory(activity.application,100)).get(
+            CommentViewModel::class.java)
+        viewModel.comments.observe(viewLifecycleOwner, Observer<List<Comment>> {
+            it.apply {
+                viewModelAdapter!!.comments = this
+                Log.i("llego aqui","hola soy api comments")
             }
+        })
+        viewModel.eventNetworkError.observe(viewLifecycleOwner, Observer<Boolean> { isNetworkError ->
+            if (isNetworkError) onNetworkError()
+        })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun onNetworkError() {
+        if(!viewModel.isNetworkErrorShown.value!!) {
+            Toast.makeText(activity, "Network Error", Toast.LENGTH_LONG).show()
+            viewModel.onNetworkErrorShown()
+        }
     }
 }

f0972b14eba23196bf04178a307e3a12c6016785
johanCarvajalAndes <j.carvajalm@uniandes.edu.co>
2021-11-30 12:41:45 -0500

    vista lista de artistas finalizada

---
+++ app/src/main/java/com/example/vinilos/ui/ListArtist.kt
@@ -0,0 +1,11 @@
+package com.example.vinilos.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListArtist : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_artist)
+    }
+}

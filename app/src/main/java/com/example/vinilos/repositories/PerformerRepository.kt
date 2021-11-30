45a6fd700b7a54a198efb48e62ae499c745d63bb
johanCarvajalAndes <j.carvajalm@uniandes.edu.co>
2021-11-30 11:10:26 -0500

    finalizado conexion al api

--- app/src/main/java/com/example/vinilos/repositories/PerformerRepository.kt
+++ app/src/main/java/com/example/vinilos/repositories/PerformerRepository.kt
@@ -8,13 +8,12 @@ import com.example.vinilos.network.NetworkServiceAdapter
 class PerformerRepository (val application: Application) {
 
     fun refreshData(
-        artistId: Int,
         callback: (List<Performer>) -> Unit,
         onError: (VolleyError) -> Unit
     ) {
         //Determinar la fuente de datos que se va a utilizar. Si es necesario consultar la red, ejecutar el siguiente código
         NetworkServiceAdapter.getInstance(application).getPerformerDetail(
-            artistId, {
+            {
                 //Guardar los coleccionistas de la variable it en un almacén de datos local para uso futuro
                 callback(it)
             },
